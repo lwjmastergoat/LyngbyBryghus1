5d19ac771dffd05003fcd21246ca84dd69a5e9b6
anettemadsen <anettebmadsen@gmail.com>
2018-01-16 10:16:36 +0100

    fkjhgjfkdh

--- LyngbyBryghus/Controllers/HomeController.cs
+++ LyngbyBryghus/Controllers/HomeController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LyngbyBrygRepo.Factories;
 
 namespace LyngbyBryghus.Controllers
 {
     public class HomeController : Controller
     {
+        KontaktFac kf = new KontaktFac();
         // GET: Home
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Kontakt()
+        {
+            return View(kf.Get(1));
         }
     }
 }
