a46ccb711bcf82c813fb17f1b4b0058d776025c8
chrisellegaard <christinaandersen21@gmail.com>
2018-01-16 14:00:03 +0100

    factorypis

---
+++ LyngbyBrygRepo/Factories/NyhedsFac.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Duser;
+using LyngbyBrygRepo.Models;
+
+namespace LyngbyBrygRepo.Factories
+{
+    public class NyhedsFac:AutoFac<Nyhedstabel>
+    {
+        public List<Nyhedstabel> GetNews()
+
+            // Henter alle nyheder ordnet efter Dato, nyeste først. Joinet med bruger så Navnet på skribent fremkommer.
+        {
+            String SQL = "SELECT NyhedsTabel.ID, Overskrift, Underoverskrift, Indhold, Dato, NyhedsTabel.Image, BrugerID, BrugerTabel.Navn FROM NyhedsTabel JOIN BrugerTabel ON BrugerTabel.ID = NyhedsTabel.BrugerID ORDER BY Dato DESC";
+
+            return ExecuteSQL<Nyhedstabel>(SQL);
+        }
+
+
+    }
+}
--- LyngbyBryghus/Controllers/HomeController.cs
+++ LyngbyBryghus/Controllers/HomeController.cs
@@ -12,10 +12,15 @@ namespace LyngbyBryghus.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        OmFac Of = new OmFac();
+
         public ActionResult Index()
         {
-            return View();
+            return View(Of.GetAll());
         }
+
+        // Der skal være en Partial view (sandsynligvis), hvor nyhederne kan være, da der ikke kan være 2 return.
+
 
         ProduktFac Pf = new ProduktFac();
 
