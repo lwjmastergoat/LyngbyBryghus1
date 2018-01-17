7a3e6de20d9c65e88ee98e1a72f6718de30997ab
chrisellegaard <christinaandersen21@gmail.com>
2018-01-17 09:01:19 +0100

    lålå

--- LyngbyBryghus/Controllers/HomeController.cs
+++ LyngbyBryghus/Controllers/HomeController.cs
@@ -13,11 +13,15 @@ namespace LyngbyBryghus.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        KontaktFac kf = new KontaktFac();
+        OmFac Of = new OmFac();
+
         public ActionResult Index()
         {
-            return View();
+            return View(Of.GetAll());
         }
+
+        // Der skal være en Partial view (sandsynligvis), hvor nyhederne kan være, da der ikke kan være 2 return.
+
 
         ProduktFac Pf = new ProduktFac();
 
