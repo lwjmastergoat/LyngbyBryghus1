2e42f69915ed1947eee8970207628fc1eea76de6
anettemadsen <anettebmadsen@gmail.com>
2018-01-22 12:12:52 +0100

    lavet login side til kunder, og en til admin/bruger

--- LyngbyBrygRepo/Factories/KundeFac.cs
+++ LyngbyBrygRepo/Factories/KundeFac.cs
@@ -5,10 +5,37 @@ using System.Text;
 using System.Threading.Tasks;
 using LyngbyBrygRepo.Models;
 using Duser;
+using LyngbyBrygRepo;
+using System.Data.SqlClient;
 
 namespace LyngbyBrygRepo.Factories
 {
-    public class KundeFac
+    public class KundeFac : AutoFac<KundeTabel>
     {
+        public KundeTabel Login(string Navn, string Password)
+        {
+            string SQL = "SELECT * FROM KundeTabel WHERE Navn=@Navn AND Password=@Password";
+
+            using (SqlCommand cmd = new SqlCommand(SQL, Conn.CreateConnection()))
+            {
+                cmd.Parameters.AddWithValue("@Navn", Navn);
+                cmd.Parameters.AddWithValue("@Password", Password);
+
+                Mapper<KundeTabel> mapper = new Mapper<KundeTabel>();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                KundeTabel kunde = new KundeTabel();
+
+                if (reader.Read())
+                {
+                    kunde = mapper.Map(reader);
+                }
+
+                reader.Close();
+                cmd.Connection.Close();
+
+                return kunde;
+            }
+        }
     }
 }
---
+++ LyngbyBryghus/Controllers/KundeLoginController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LyngbyBrygRepo.Models;
+using LyngbyBrygRepo.Factories;
+using LyngbyBrygRepo;
+
+namespace LyngbyBryghus.Controllers
+{
+    public class KundeLoginController : Controller
+    {
+        KundeFac kf = new KundeFac();
+        // GET: KundeLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Navn, string Password)
+        {
+            KundeTabel kunde = kf.Login(Navn, Password);
+
+            if (kunde.Navn != null)
+            {
+                Session["id"] = kunde.ID;
+                Session["abonnent"] = kunde.Abonnent;
+                return Redirect("/Kunde/");
+            }
+            else
+            {
+                ViewBag.Msg = "<b>Forkert login</b>";
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Remove("id");
+            Session.Remove("abonnent");
+            return Redirect("/Home/Index/");
+        }
+    }
+}
